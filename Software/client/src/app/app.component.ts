c7a09abdfac5b3f815a52b5c06ef604d630d2c68
sematicbits <sairaj.kotagiri@india.semanticbits.com>
2018-03-06 20:24:33 +0530

    Sticky SideBar

--- Software/client/src/app/app.component.ts
+++ Software/client/src/app/app.component.ts
@@ -7,19 +7,5 @@ import * as $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  public ngOnInit() {
-    $(document).ready(function() {
-      const sidebar = $('.sidebar');
-      const top = sidebar.offset().top - parseFloat(sidebar.css('margin-top'));
-
-      $(window).scroll(function (event) {
-        const y = $(this).scrollTop();
-        if (y >= top) {
-          sidebar.addClass('fixed');
-        } else {
-          sidebar.removeClass('fixed');
-        }
-      });
-    });
-  }
+  public ngOnInit() {  }
 }
